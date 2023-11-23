6d0b9fe13a420475ec36c24a43938188025952fa
akademiyaTOP <dk26.01.1999@yandex.ru>
2023-11-23 14:36:14 +0300

    ghgg

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int c = 0;
+for (int i = 0; i < 10; i++)
+    Console.WriteLine("Hello,fgfg!");
